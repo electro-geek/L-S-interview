eb329a882fe2e19cba13cb2470c10f55cba773ad
Mritunjay Sharma <57140997+electro-geek@users.noreply.github.com>
2024-05-07 04:01:57 +0000

    input taking

--- Intro.java
+++ Intro.java
@@ -1,18 +1,25 @@
 import java.util.*;
 public class Intro{
     public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
         int[][] Arr1 = new int[3][3];
         int[][] Arr2 = new int[3][3];
         int[][] multi = new int[3][3];
+        System.out.println("Enter the values for of Array 1");
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                Arr1[i][j] = scan.nextInt();
+            }
+        }
         for(int i = 0; i < 3; i++){
             for(int j = 0; j < 3; j++){
                 if(i==j){
-                    Arr1[i][j] = 1;
+                    // Arr1[i][j] = 1;
                     Arr2[i][j] = 1;
                 }
                 else{
-                    Arr1[i][j] = 2;
-                    Arr2[i][j] = 2;
+                    // Arr1[i][j] = 3;
+                    Arr2[i][j] = 1;
                 }
             }
         }
